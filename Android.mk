676deb5de3354a33193923851d1f652b78838050
Saith Kay <kaysaith@sina.com>
2017-11-07 15:08:39 +0800

    【bug】修复王勇恢复代码导致的回退按钮不可用的问题

---
+++ Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS += -D__ANDROID__ -g
+#APP_PLATFORM := android-19
+
+LOCAL_C_INCLUDES =	\
+	$(LOCAL_PATH)/../3rdparty/libzip  \
+	$(LOCAL_PATH)/../bs \
+	$(LOCAL_PATH)/
+
+SRCFILES = $(wildcard $(LOCAL_PATH)/*.cpp $(LOCAL_PATH)/*/*.cpp)
+SRCS = $(patsubst $(LOCAL_PATH)/%, ./%,$(SRCFILES))
+
+LOCAL_SRC_FILES += \
+	$(SRCS)
+
+#LOCAL_SHARED_LIBRARIES := libnative
+#LOCAL_LDLIBS    := -llog -landroid -lz -ldl
+
+LOCAL_MODULE := basic
+
+include $(BUILD_STATIC_LIBRARY)
